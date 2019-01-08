2d31f4f4e66ded15cf852b9cf80d8a71d60964ef
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-07 19:57:41 -0800

    Complete intro to the game.

---
+++ DN_Lab01_AboutMeQuiz/DN_Lab01_AboutMeQuiz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DN_Lab01_AboutMeQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Jason's Quiz");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}! Press Enter when your ready to play.");
+            Console.ReadLine();         
+        }
+        
+    }
+}
