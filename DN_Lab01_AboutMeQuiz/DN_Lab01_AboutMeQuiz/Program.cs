4076ce961adaa0b69f54f115bd542b18e3ee9d28
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-07 20:13:59 -0800

    Added questions array, answers array, and score variable.

--- DN_Lab01_AboutMeQuiz/DN_Lab01_AboutMeQuiz/Program.cs
+++ DN_Lab01_AboutMeQuiz/DN_Lab01_AboutMeQuiz/Program.cs
@@ -8,9 +8,26 @@ namespace DN_Lab01_AboutMeQuiz
         {
             //Questions
 
+            string[] questions = {
+                "What was my profession before attending Code Fellows?",
+                "True or False: Do I own a dog?",
+                "Do I live in Kirkland or Bothell?",
+                "True or False: Do I own a truck?",
+                "How many states have I lived in? "
+            };
+
             //Answers
 
+            string[] answers = {
+                "Geologist",
+                "True",
+                "Bothell",
+                "True",
+                "3"
+            };
+
             //Score
+            int score = 0;
 
             // Game Introduction
             Console.WriteLine("Welcome to Jason's Quiz");
